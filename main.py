831a936333d5b720c85d533733cca53f59c4a763
haoenlu <haoenlu.07@gmail.com>
2024-05-05 01:14:04 -0400

    update

--- main.py
+++ main.py
@@ -62,18 +62,19 @@ def train(args,config):
     
     train_iter = get_infinite_batch(dataloader)
 
-    print("Start Training encoder-decoder")
-    # encoder-decoder training loop
-    for step in range(config.training.ed_iters):
-        batch = next(train_iter).to(config.device).float().permute(0,2,1)
-        recon_loss = ed_step(state,batch)
 
-        if step % config.training.log_freq == 0:
-            print(f"Step: {step}, Loss: {recon_loss.item()}")
-            writer.add_scalar("Reconstruction Training Loss",recon_loss.item(),step)
+    # print("Start Training encoder-decoder")
+    # # encoder-decoder training loop
+    # for step in range(config.training.ed_iters):
+    #     batch = next(train_iter).to(config.device).float().permute(0,2,1)
+    #     recon_loss = ed_step(state,batch)
+
+    #     if step % config.training.log_freq == 0:
+    #         print(f"Step: {step}, Loss: {recon_loss.item()}")
+    #         writer.add_scalar("Reconstruction Training Loss",recon_loss.item(),step)
         
-        if step != 0 and step % config.training.checkpoint_freq == 0:
-            checkpoint.save_checkpoint(os.path.join(args.ckptDir,f'checkpoint_{step}'),state)
+    #     if step != 0 and step % config.training.checkpoint_freq == 0:
+    #         checkpoint.save_checkpoint(os.path.join(args.ckptDir,f'checkpoint_{step}'),state)
 
 
     print("Start Training score denoiser")
